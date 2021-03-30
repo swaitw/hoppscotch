10c67895469f92cfdc9db00e55a9e0f9d4512c56
Liyas Thomas <liyascthomas@gmail.com>
2021-03-30 01:30:35 +0000

    feat: eslint + stylelint

--- nuxt.config.js
+++ nuxt.config.js
@@ -103,6 +103,10 @@ export default {
   buildModules: [
     // https://github.com/nuxt-community/pwa-module
     "@nuxtjs/pwa",
+    // https://github.com/nuxt-community/eslint-module
+    "@nuxtjs/eslint-module",
+    // https://github.com/nuxt-community/stylelint-module
+    "@nuxtjs/stylelint-module",
     // https://github.com/nuxt-community/analytics-module
     "@nuxtjs/google-analytics",
     // https://github.com/nuxt-community/gtm-module
